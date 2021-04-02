f1bee8bda2c6edcde39bd3f141a4a0df4f5de2ac
HoangNam-Phan <hoangnamphan@outlook.de>
2021-04-02 17:03:43 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Bootstrap from 'bootstrap/dist/css/bootstrap.min.css';
+
+const App = () => {
+    return (
+        <div>
+            <h1>herro</h1>
+        </div>
+    )
+}
+
+export default App
